b966d54ca965425feb92946174937051e380dabb
devil <devil@DESKTOP-QBITRB3>
2021-01-26 20:27:11 +0600

    basic project

--- src/basicprogram/BasicProgram.java
+++ src/basicprogram/BasicProgram.java
@@ -22,10 +22,37 @@ public class BasicProgram {
         
         
     }
+    /*
+    task2:Read a number n from user and find min and max and print min-max 
+    */
+    public static void task2(){
+       Scanner  sc = new Scanner(System.in);
+       System.out.printf("Enter  integer n:\n");
+       int n=sc.nextInt();
+       int min = Integer.MAX_VALUE;
+       int max =Integer.MIN_VALUE;      
+       for(int i =0;i<=n;i++)
+       {
+           int num =sc.nextInt();
+           if(num<min){
+               min=num;
+           }
+           if(num>max){
+               max=num;
+           }
+          
+       }
+        int distance =max-min ;
+        System.out.printf("Largest number :%d\n",distance);
+
+        
+        
+    }
 
    
     public static void main(String[] args) {
-        task1();
+        //task1();
+        task2();
         
         
         
