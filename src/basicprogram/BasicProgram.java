5ff7f5511f38317e7fb9522e7dd08e27160f6333
devil <devil@DESKTOP-QBITRB3>
2021-01-26 19:21:48 +0600

    basic project

--- src/basicprogram/BasicProgram.java
+++ src/basicprogram/BasicProgram.java
@@ -1,21 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package basicprogram;
 
-/**
- *
- * @author devil
- */
-public class BasicProgram {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class BasicProgram {
+    /*
+    task1:input a and b two integer number from user and
+     print the summation of the number
+    */
+    public static void task1()
+    {
+        Scanner  sc = new Scanner(System.in);
+        System.out.printf("Enter two integer num:\n");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum=0;
+        for(int i =a;i<=b;i++){
+            sum += i;
+        }
+        System.out.printf("Summation :%d\n",sum);
+        
+        
+        
+    }
+
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        task1();
+        
+        
+        
     }
     
 }
